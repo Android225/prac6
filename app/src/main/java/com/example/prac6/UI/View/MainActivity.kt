bf712cdf45c48ebd16aee050c07d9a7b0b6f7dfe
Andry <a.krishtofor@gmail.com>
2023-11-15 03:52:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/prac6/UI/View/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.kotlinpract1.UI.View
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import com.example.kotlinpract1.R
+import com.example.kotlinpract1.UI.StateHolder.ViewModel.MyViewModel
+
+class MainActivity : AppCompatActivity() {
+    // init pract6
+
+    private lateinit var navController: NavController
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_container)
+
+        val viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
+        viewModel.data.observe(this, Observer { data ->
+            // обновление UI с новыми данными
+        })
+    }
+}
